abf0d93a44b658f7348d8e20536e01e7c3bb85dc
Aryan Dev Shourie <78311368+Aryan2727-debug@users.noreply.github.com>
2022-06-24 00:02:04 +0530

    Add files via upload

---
+++ SumOfStackElements.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <stack>
+using namespace std;
+int n=4;
+class Stack{
+    int *arr;
+    int top;
+    public:
+    Stack(){
+        arr=new int[n];
+        top=-1;
+    }
+    void pop(){
+        if(top==-1){
+            cout<<"The Stack is empty"<<endl;
+            return;
+        }
+        top--;
+    }
+    void push(int x){
+        if(top==n-1){
+            cout<<"The Stack is full"<<endl;
+            return;
+        }
+        top++;
+        arr[top]=x;
+    }
+    int print(){
+        if(top==-1){
+            cout<<"The Stack is empty"<<endl;
+            return -1;
+        }
+        return arr[top];
+    }
+    int getSum(){
+        if(top==-1){
+            cout<<"The Stack is empty"<<endl;
+            return -1;
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+        }
+        return sum;
+    }
+};
+int main()
+{
+    cout<<"The Stack is"<<endl;
+    Stack s;
+    s.push(10);
+    s.push(40);
+    s.push(20);
+    s.push(30);
+    cout<<"The sum of Stack elements is = "<<s.getSum()<<endl;
+    cout<<s.print()<<endl;
+    s.pop();
+    cout<<s.print()<<endl;
+    s.pop();
+    cout<<s.print()<<endl;
+    s.pop();
+    cout<<s.print()<<endl;
+    s.pop();
+    s.pop();
+    return 0;
+}
